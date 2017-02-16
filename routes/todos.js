c8af713d1268c93fa261d437d3c72631aadd26e6
PavloM7896 <pasha80676222267@gmail.com>
2017-02-16 23:05:00 +0200

    Update todos.js

--- routes/todos.js
+++ routes/todos.js
@@ -4,23 +4,25 @@ var Todo = require('../models/Todo.js');
 
 /* GET /todos listing. */
 router.get('/', function(req, res, next) {
-    Todo.find(function (err, todos) {
-        if (err) return next(err);
-        //res.writeHead(200, {'Content-Type': 'application/json'});
-        //res.send( todos, 200 );
-        res.setHeader('Access-Control-Allow-Origin','http://localhost:3000')
-        res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE')
-        res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type')
-        res.setHeader('Access-Control-Allow-Credentials',true)
-        res.status(200).json(todos);
-    });
+  Todo.find(function (err, todos) {
+    if (err) return next(err);
+    res.setHeader('Access-Control-Allow-Origin','http:/localhost:3000')
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE')
+    res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials',true)
+    res.status(200).json(todos);
+  });
 });
 
 /* POST /todos */
 router.post('/', function(req, res, next) {
   Todo.create(req.body, function (err, post) {
     if (err) return next(err);
-    res.json(post);
+    res.setHeader('Access-Control-Allow-Origin','http:/localhost:3000')
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE')
+    res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials',true)
+    res.status(200).json(post);
   });
 });
 
@@ -28,7 +30,11 @@ router.post('/', function(req, res, next) {
 router.get('/:id', function(req, res, next) {
   Todo.findById(req.params.id, function (err, post) {
     if (err) return next(err);
-    res.json(post);
+    res.setHeader('Access-Control-Allow-Origin','http:/localhost:3000')
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE')
+    res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials',true)
+    res.status(200).json(post);
   });
 });
 
@@ -36,7 +42,11 @@ router.get('/:id', function(req, res, next) {
 router.put('/:id', function(req, res, next) {
   Todo.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
-    res.json(post);
+    res.setHeader('Access-Control-Allow-Origin','http:/localhost:3000')
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE')
+    res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials',true)
+    res.status(200).json(post);
   });
 });
 
@@ -44,7 +54,11 @@ router.put('/:id', function(req, res, next) {
 router.delete('/:id', function(req, res, next) {
   Todo.findByIdAndRemove(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
-    res.json(post);
+    res.setHeader('Access-Control-Allow-Origin','http:/localhost:3000')
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE')
+    res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials',true)
+    res.status(200).json(post);
   });
 });
 
